fab4015287e750dc9fc39b526204c50655ef3628
zohoMatt <mattzuo12@gmail.com>
2020-12-03 12:03:01 -0500

    chores: Minor fixes.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,5 @@ import { logger } from './utils/logger';
 
 // Init Sentry & Capture Global Error
 Sentry.init({ dsn: process.env.SENTRY });
-// app.on('error', (err, ctx) => {
-//     Sentry.withScope((scope) => {
-//         scope.addEventProcessor((event) => Sentry.Handlers.parseRequest(event, ctx.request));
-//         Sentry.captureException(err);
-//     });
-// });
 
 logger.info('Application running...');
